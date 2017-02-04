d972fce08d872e1b77e56bc337c195b1230bbf9e
sagandhi95 <sohamgandhi95@gmail.com>
2017-02-04 11:44:22 +0530

    KeyGen method updated

--- src/KeyGen.java
+++ src/KeyGen.java
@@ -1,13 +1,15 @@
 import java.util.Random;
 
 public class KeyGen {
-	public static void main(String[] args) {
+	public static String generateKey() {
 		Random r = new Random();
 		int Low = 0;
 		int High = 2;
+		StringBuilder key = new StringBuilder("");
 		for(int i=0;i<1028;i++){
 			int Result = r.nextInt(High-Low) + Low;
-			System.out.print(Result);
+			key.append(Result);
 		}
+		return key.toString();
 	}
 }
