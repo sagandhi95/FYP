400699931bb05e0c1072fedaa8a10d3767c4d1be
pavanbirajdar22 <Pavan@Pavans-MacBook-Air.local>
2017-02-04 09:47:00 +0530

    Code added by Chinmay
    Table shifting

--- src/TableGen.java
+++ src/TableGen.java
@@ -2,15 +2,58 @@ import java.util.Random;
 
 public class TableGen {
 	public static void main(String[] args) {
+		int matrix[][] = new int[16][16];
+
 		Random r = new Random();
 		int Low = 1;
-		int High = 128;
-		for(int i=0;i<16;i++){
-			for(int j=0;j<16;j++){
-				int Result = r.nextInt(High-Low) + Low;
-				System.out.print(Result+"\t");
+		int High = 10;
+
+		// Generate Random matrix.
+		for (int i = 0; i < 16; i++) {
+			for (int j = 0; j < 16; j++) {
+
+				int Result = r.nextInt(High - Low) + Low;
+				matrix[i][j] = Result;
+				System.out.print(matrix[i][j] + "\t");
 			}
 			System.out.println();
 		}
+
+		// Shift row wise
+		for (int i = 0; i < 16; i++) {
+			rowshiftRight(i, matrix);
+		}
+		System.out.println();
+
+		// Print matrix
+		for (int i = 0; i < 16; i++) {
+			for (int j = 0; j < 16; j++) {
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println();
+		}
+
+		// Combining 2 elements of matrix.
+		int combined_array[] = new int[128];
+		int k = 0;
+		for (int i = 0; i < 16; i++) {
+			for (int j = 0; j < 16; j += 2) {
+				combined_array[k] = matrix[i][j] * 10 + matrix[i][j + 1];
+				k++;
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < 128; i++)
+			System.out.print(combined_array[i] + " ");
+
+	}
+
+	public static void rowshiftRight(int i, int[][] array) {
+		int m = array[i].length;
+		int temp = array[i][m - 1];
+		for (int k = m - 1; k >= 1; k--) {
+			array[i][k] = array[i][k - 1];
+		}
+		array[i][0] = temp;
 	}
 }
